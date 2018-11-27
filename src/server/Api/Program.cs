6e62cd66f611cf7e174ec3b868b2a060de370354
Lucian Sturião <luciansr@gmail.com>
2018-11-26 23:05:23 -0200

    adding logs do project

--- src/server/Api/Program.cs
+++ src/server/Api/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace Api
 {
@@ -20,6 +21,7 @@ namespace Api
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseKestrel()
+                .UseNLog()
                 .UseStartup<Startup>();
     }
 }
